7b913073a2f5fd21883577ddc138a4e7a6a46d20
Kejin Zhou <kejin.zhou@aalto.fi>
2026-03-20 14:04:41 +0200

    app: rewrites screen changing logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import StartupScreen from "./components/StartupScreen";
 import ConfigScreen from "./components/ConfigScreen.tsx";
 import ProgressScreen from "./components/ProgressScreen.tsx";
+import CustomButton from "./components/custom-button.tsx";
 
 interface Config {
   coreType: string | null;
@@ -10,29 +11,30 @@ interface Config {
 }
 
 const App = () => {
-  const [screen, setScreen] = useState<"startup" | "config" | "progress">("startup");
+  // 0 starting up, 1 config, 2 buttons panel, 9 processing
+  const [screen, setScreen] = useState<Number>(0); // <"startup" | "config" | "progress">("startup");
   const [config, setConfig] = useState<Config | null>(null);
 
   const handleInit = () => {
     setConfig(null);
-    setScreen("config");
+    setScreen(1);
   };
 
   const handleStart = (cfg: Config) => {
     setConfig(cfg);
-    setScreen("progress");
+    setScreen(9);
   };
 
   const handleReset = () => {
     setConfig(null);
-    setScreen("config");
+    setScreen(1);
   };
 
   return (
     <div className="size-full flex items-center justify-center px-8">
-      {screen === "startup" && <StartupScreen onClick={handleInit} />}
-      {screen === "config" && <ConfigScreen onStart={handleStart} />}
-      {screen === "progress" && config && (
+      {screen === 0 && <StartupScreen onClick={handleInit} />}
+      {screen === 1 && <ConfigScreen onStart={handleStart} />}
+      {screen === 9 && config && (
         <ProgressScreen config={config} onReset={handleReset} />
       )}
     </div>
