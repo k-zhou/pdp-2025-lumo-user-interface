61321e30ae92b510ed47dfc036f5e00840151233
Kejin Zhou <kejin.zhou@aalto.fi>
2026-04-01 13:05:26 +0300

    button panel: adds directional buttons

--- src/components/ButtonPanelScreen.tsx
+++ src/components/ButtonPanelScreen.tsx
@@ -16,11 +16,50 @@ const handleButton1Post = async () => {
     console.log(`The response: ${r}`);
 };
 
+const handleButtonHome = async () => {
+    const r = await fetch(`${PUBLIC_API_URL}/press-home`, {method: "POST"});
+    console.log(`The response: ${r}`);
+};
+
+const handleButtonYMinus = async () => {
+    const r = await fetch(`${PUBLIC_API_URL}/press-y-minus`, {method: "POST"});
+    console.log(`The response: ${r}`);
+};
+
+const handleButtonYPlus = async () => {
+    const r = await fetch(`${PUBLIC_API_URL}/press-y-plus`, {method: "POST"});
+    console.log(`The response: ${r}`);
+};
+
+const handleButtonXMinus = async () => {
+    const r = await fetch(`${PUBLIC_API_URL}/press-x-minus`, {method: "POST"});
+    console.log(`The response: ${r}`);
+};
+
+const handleButtonXPlus = async () => {
+    const r = await fetch(`${PUBLIC_API_URL}/press-x-plus`, {method: "POST"});
+    console.log(`The response: ${r}`);
+};
+
 const ButtonPanelScreen = () => {
     return (
-        <div>
-            {CustomButton({onClick:handleButton1Get,  label:"Button 1, Get"})}
-            {CustomButton({onClick:handleButton1Post, label:"Button 1, Post"})}
+        <div className="flex flex-col gap-4">
+            <div className="flex grid grid-cols-3 gap-4">
+                {CustomButton({onClick:handleButtonHome, label:"Homing", className:"flex-auto gap-4  h-[128px] bg-[#393939]" })}
+                {CustomButton({onClick:handleButtonYMinus, label:"Y Minus", className:"flex-auto gap-4  h-[128px]" })}
+                {CustomButton({onClick:() => null, label:" ", className:"flex-auto gap-4  h-[128px] bg-[#696969]"})}
+                {CustomButton({onClick:handleButtonXPlus, label:"X Plus", className:"flex-auto gap-4  h-[128px]" })}
+                {CustomButton({onClick:() => null, label:" ", className:"flex-auto gap-4  h-[128px] bg-[#696969]"})}
+                {CustomButton({onClick:handleButtonXMinus, label:"X Minus", className:"flex-auto gap-4  h-[128px]" })}
+                {CustomButton({onClick:() => null, label:" ", className:"flex-auto gap-4  h-[128px] bg-[#696969]"})}
+                {CustomButton({onClick:handleButtonYPlus, label:"Y Plus", className:"flex-auto gap-4  h-[128px]" })}
+                {CustomButton({onClick:() => null, label:" ", className:"flex-auto gap-4  h-[128px] bg-[#696969]"})}
+            </div>
+            <hr />
+            <div className="grid grid-cols-3 gap-4">
+                {CustomButton({onClick:handleButton1Get,  label:"Button 1, Get", className:"flex-auto gap-4  h-[64px]" })}
+                {CustomButton({onClick:handleButton1Post, label:"Button 1, Post", className:"flex-auto gap-4  h-[64px]" })}
+            </div>
         </div>
     );
 };
