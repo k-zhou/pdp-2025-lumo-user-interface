919413f70b72d7be0bb2527fa3b5f8e4f57af68a
Kejin Zhou <kejin.zhou@aalto.fi>
2026-04-01 13:05:06 +0300

    custom button: styling

--- src/components/custom-button.tsx
+++ src/components/custom-button.tsx
@@ -1,17 +1,31 @@
 interface ButtonProps {
   onClick: () => void;
   label?: string;
+  className?: string;
 }
 
-export default function CustomButton({ onClick, label }: ButtonProps) {
+const definedStyles = new Map();
+definedStyles
+  .set(0, "bg-[#08f] content-stretch flex      \
+          h-[47px] items-center justify-center \
+          px-[13px] relative rounded-[100px]   \
+          shrink-0 w-[382px] cursor-pointer    \
+          hover:opacity-90 transition-opacity")
+  .set(1, "");
+
+export default function CustomButton({ onClick, label, className }: ButtonProps) {
   return (
-    <div className="content-stretch flex flex-col items-center justify-center relative shrink-0">
+    <div className="content-stretch flex-1 items-center 
+      justify-center relative shrink-0">
       <button
         onClick={onClick}
-        className="bg-[#08f] content-stretch flex h-[47px] items-center justify-center px-[13.429px] relative rounded-[100px] shrink-0 w-[382.714px] cursor-pointer hover:opacity-90 transition-opacity"
+        className={definedStyles.get(0) + className }
       >
-        <div className="flex flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] relative shrink-0 text-[21.82px] text-center text-white whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>
-          <p className="leading-[26.857px]">{ label ||"Confirm"}</p>
+        <div className="flex flex-col font-['SF_Pro:Medium',sans-serif] 
+          font-[510] justify-center leading-[0] relative shrink-0 text-[22px] 
+          text-center text-white whitespace-nowrap" 
+          style={{ fontVariationSettings: "'wdth' 100" }}>
+          <p className="leading-[27px]">{ label ||"Confirm"}</p>
         </div>
       </button>
     </div>
