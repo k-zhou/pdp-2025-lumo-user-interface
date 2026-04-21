4a5b7ed1fcf2bf9ccf81f2bf446336cacfefc3f6
Kejin Zhou <kejin.zhou@aalto.fi>
2026-04-21 16:48:58 +0300

    modifies vite config to allow aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from "node:path";
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,4 +9,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    },
+  },
 })
